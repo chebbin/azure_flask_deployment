8d6aa59f581b77d682188b4dbf79866717dee341
chebbin <chevi.ebbin@stonybrook.edu>
2023-09-18 23:12:38 +0000

    website that has a table

--- app2.py
+++ app2.py
@@ -7,15 +7,16 @@ app = Flask(__name__)
 def index():
     return render_template('base.html')
 
-df = pd.read_csv ('/home/chevi_ebbin/azure_flask_deployment/ComprehensiveJR.csv')
+
+df = pd.read_csv('https://raw.githubusercontent.com/chebbin/azure_flask_deployment/main/ComprehensiveJR.csv')
 @app.route('/data')
-def data(data = df):
+def data(data=df):
     data = data.sample(15)
-    return render_template('base.html', data = data)
+    return render_template('data.html', data=data)
 
 @app.route('/chart')
 def chartpage():
-    df = pd.read_csv ('/home/chevi_ebbin/azure_flask_deployment/ComprehensiveJR.csv')
+    df = pd.read_csv ('https://raw.githubusercontent.com/chebbin/azure_flask_deployment/main/ComprehensiveJR.csv')
     return render_template('chart.html', chart = df)
 
 
